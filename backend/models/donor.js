dfc3fac6d9acb04d7ca729e6fa4a2b32d5258ab4
Laraib Fatima <flaraib691@gmail.com>
2026-05-18 18:26:14 +0500

    backend updated

--- backend/models/donor.js
+++ backend/models/donor.js
@@ -1,25 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const donorSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true
-    },
-    bloodGroup : {
-        type  : String,
-        required : true
-    },
-    city : {
-        type : String,
-        required : true
-    },
-    phone : {
-        type : String,
-        required : true
-    },
-    age : {
-        type : Number,
-        required : true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  phone: {
+    type: String,
+    required: true,
+  },
+
+  bloodGroup: {
+    type: String,
+    required: true,
+  },
+
+  city: {
+    type: String,
+    required: true,
+  },
+
+  // ADD THESE FIELDS
+  availability: {
+    type: String,
+    default: "Available",
+  },
+
+  lastDonation: {
+    type: String,
+  },
 });
-module.exports = mongoose.model('Donor' , donorSchema);
+
+module.exports = mongoose.model("Donor", donorSchema);
