df7d7bbd58de81d838f8de1717011d8d289a49fd
Laraib Fatima <flaraib691@gmail.com>
2026-05-18 20:20:13 +0500

    updated frontend

--- backend/models/donor.js
+++ backend/models/donor.js
@@ -6,11 +6,6 @@ const donorSchema = new mongoose.Schema({
     required: true,
   },
 
-  email: {
-    type: String,
-    required: true,
-  },
-
   phone: {
     type: String,
     required: true,
